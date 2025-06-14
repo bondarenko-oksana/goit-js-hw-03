29cfa153d2bce49114eacca8a93c0658e2ee8526
Oksana <familianamy@gmail.com>
2025-06-14 20:26:53 +0300

    delete equals sign

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     let result = [];
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
           result.push(numbers[i]);
         }
@@ -15,3 +15,5 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
